7dcfab2e122d3c5e781e49afc14acb6a8b39e5f6
sirajul83 <sirajulislam8354@gmail.com>
2023-01-30 22:39:55 +0600

    contact issue

--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -5,14 +5,14 @@
     <hr />
     <div class="row">
         <div class="col-md-6">
-            <form style="padding-left: 15px">
+            <form style="padding-left: 15px" method="post" action="">
                 <div class="form-group">
-                    <label for="exampleInputEmail1">Name </label>
-                    <input type="text" class="form-control" id="exampleInputEmail1"  placeholder="Enter Name">
+                    <label for="exampleInputName">Name </label>
+                    <input type="text" name="name" class="form-control" id="exampleInputNAme"  placeholder="Enter Name">
                   </div>
                 <div class="form-group">
                   <label for="exampleInputEmail1">Email</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1"  placeholder="Enter email">
+                  <input type="email" name="email" class="form-control" id="exampleInputEmail1"  placeholder="Enter email">
                 </div>
                 <div class="form-group">
                   <label for="message">Message</label>
